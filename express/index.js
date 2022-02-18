bf355f04c0bf7611f653578f743abce8a32f6897
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-02-18 15:50:46 -0300

    Routes

--- express/index.js
+++ express/index.js
@@ -3,6 +3,19 @@ const express = require( 'express' )
 // init express
 const app = express()
 
+//function callback
+app.get('/', ( req, res ) => {
+     res.send( 'Welcome guide programmer' ) 
+})
+
+app.get( '/blog', ( req, res ) => {
+    res.send( 'Welcome my blog')
+}) 
+
+app.get( '/canal/youtube', ( req, res ) => {
+    res.send( 'Welcome youtube' )
+})
+
 app.listen(4000, (err ) =>   {
     if(err) { 
     console.log( err ) 
