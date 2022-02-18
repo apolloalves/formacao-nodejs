74bd7ae60a874c92a9efad9246b89f8f180ea30b
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-02-18 17:56:03 -0300

    Routes Params

--- express/index.js
+++ express/index.js
@@ -16,11 +16,26 @@ app.get( '/canal/youtube', ( req, res ) => {
     res.send( 'Welcome youtube' )
 })
 
-app.listen(4000, (err ) =>   {
-    if(err) { 
-    console.log( err ) 
-
-    }else {
-        console.log( 'Server running success' )
-    } 
+// Route param
+app.get('/ola/:name', ( req, res ) => {
+    // res => dados enviados pelo usuário
+    // req => resposta que vai ser enviada para o usuário
+    res.send( req.params.name )
 })
+
+app.get('/hello/:name', ( req, res) => {
+    const name = req.params.name
+    res.send(`HI ${name}`)
+})
+
+
+// app.listen(4000, (err ) =>   {
+//     if(err) { 
+//     console.log( err ) 
+
+//     }else {
+//         console.log( 'Server running success' )
+//     } 
+// })
+
+app.listen( 4000,() => console.log('Server running'))
